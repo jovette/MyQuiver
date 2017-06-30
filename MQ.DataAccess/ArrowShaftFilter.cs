af1b7f37e2244d6a800119497ced32b62dc8f6b5
Jovette <jovette.estrellado@gmail.com>
2017-06-30 11:33:11 -0700

    Remove the MQ folders as the are defined in the folders without the  prefix

--- MQ.DataAccess/ArrowShaftFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyQuiver.DataAccess
-{
-    public sealed class ArrowShaftFilter : Filter
-    {
-        public string ArrowMaterialType { get; set; }
-
-        public double? ManufacturedShaftLength { get; set; }
-    }
-}
